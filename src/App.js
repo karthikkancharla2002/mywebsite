00cced1cffcc32f1ed19965a18bcac1dc9b15ef8
karthikkancharla2002 <karthikkancharla2002@gmail.com>
2021-06-17 22:16:19 +0530

    Deploying to github pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Hello! I am Karthik and welcome to my website. It is under construction, so please don't mind.</h1>
     </div>
   );
 }
