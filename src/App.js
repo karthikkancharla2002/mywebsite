af4848cb9be106d8f078d62320b85f62f841a745
karthikkancharla2002 <karthikkancharla2002@gmail.com>
2021-06-18 02:02:39 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -29,11 +29,11 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-            <Nav.Link href="/#/">Home</Nav.Link>
-            <Nav.Link href="/#/about">About Me</Nav.Link>
+            <Nav.Link href="#/">Home</Nav.Link>
+            <Nav.Link href="#/about">About Me</Nav.Link>
             <Nav.Link href="https://drive.google.com/file/d/1hRV98JtopqQmRxdi6AxcYGeqkru9rXmO/view?usp=sharing">Resume</Nav.Link>
-            <Nav.Link href="/#/education">Education</Nav.Link>
-            <Nav.Link href="/#/skills">Skills</Nav.Link>
+            <Nav.Link href="#/education">Education</Nav.Link>
+            <Nav.Link href="#/skills">Skills</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </div>
